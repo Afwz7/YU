e7bbc80ce8b6e22334e07c0e5dd501c57bdcd283
Afwz7 <ahmedelfaouz3@gmail.com>
2023-09-25 18:22:59 -0400

    the answer is simple : ME!

--- java/Wind_Chill_Temperature.java
+++ java/Wind_Chill_Temperature.java
@@ -18,19 +18,19 @@ class Wind_Chill_Temperature
 
 		twc = 0;
 		Scanner _var1 = new Scanner(System.in);
-		System.out.printf("Enter the temperature in Fahrenheit between -58°F and 41°F");
+		System.out.printf("Enter the temperature in Fahrenheit between -58°F and 41°F : ");
 		ta = _var1.nextFloat();
-		System.out.printf("Enter the wind speed (>= 2) in miles per hour :");
+		System.out.printf("Enter the wind speed (>= 2) in miles per hour : ");
 		v = _var1.nextFloat();
 		_var1.close();
 		if (v >= 2)
 		{
 			twc = 35.74 + 0.6215 * ta + (-35.75) * Math.pow(v, 0.16) + 0.4275 * ta * Math.pow(v, 0.16);
-			System.out.printf("the wind chill index is : %f", twc);
+			System.out.printf("the wind chill index is : %f\n", twc);
 		}
 		else
 		{
-			System.out.printf("error -1 [wind speed <= 2]");
+			System.out.printf("error -1 [wind speed <= 2]\n");
 		}
 	}
 	public static void main(String[] arguments)
